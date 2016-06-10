6504babfc83bacb16da4c7f1cc95b17f64f42b51
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-06-10 16:51:47 -0700

    add fake data

---
+++ example/app/data/phone-book.js
@@ -0,0 +1,347 @@
+module.exports = [
+  {
+    "name": "Susie Hewitt",
+    "phone": "+1 (826) 534-2082"
+  },
+  {
+    "name": "Willa Reeves",
+    "phone": "+1 (993) 475-2371"
+  },
+  {
+    "name": "Daugherty Blevins",
+    "phone": "+1 (832) 534-3802"
+  },
+  {
+    "name": "Garcia Murphy",
+    "phone": "+1 (881) 430-3471"
+  },
+  {
+    "name": "Roberson Roach",
+    "phone": "+1 (946) 478-2715"
+  },
+  {
+    "name": "Sharron Morin",
+    "phone": "+1 (865) 430-2661"
+  },
+  {
+    "name": "Flynn Kidd",
+    "phone": "+1 (845) 586-3900"
+  },
+  {
+    "name": "Ethel Silva",
+    "phone": "+1 (810) 425-3777"
+  },
+  {
+    "name": "Deana Fulton",
+    "phone": "+1 (928) 465-2227"
+  },
+  {
+    "name": "Jane Gallagher",
+    "phone": "+1 (952) 500-3903"
+  },
+  {
+    "name": "Garrett Nunez",
+    "phone": "+1 (977) 441-3619"
+  },
+  {
+    "name": "Mcpherson Hickman",
+    "phone": "+1 (875) 514-3156"
+  },
+  {
+    "name": "Coleen Cantrell",
+    "phone": "+1 (844) 441-2062"
+  },
+  {
+    "name": "Gibson Rasmussen",
+    "phone": "+1 (944) 428-3848"
+  },
+  {
+    "name": "Henson Garner",
+    "phone": "+1 (999) 589-2032"
+  },
+  {
+    "name": "Candace Higgins",
+    "phone": "+1 (938) 423-2312"
+  },
+  {
+    "name": "Berg Howell",
+    "phone": "+1 (928) 442-2621"
+  },
+  {
+    "name": "Colleen Woods",
+    "phone": "+1 (916) 416-3810"
+  },
+  {
+    "name": "Vicki Cortez",
+    "phone": "+1 (986) 487-3857"
+  },
+  {
+    "name": "Mooney Avila",
+    "phone": "+1 (974) 472-3766"
+  },
+  {
+    "name": "Shepard Santiago",
+    "phone": "+1 (800) 433-3551"
+  },
+  {
+    "name": "Alyssa Solis",
+    "phone": "+1 (820) 448-2854"
+  },
+  {
+    "name": "Vanessa Stafford",
+    "phone": "+1 (975) 559-3596"
+  },
+  {
+    "name": "Joy Knapp",
+    "phone": "+1 (943) 503-3371"
+  },
+  {
+    "name": "Avery Horn",
+    "phone": "+1 (852) 501-2356"
+  },
+  {
+    "name": "Gentry Jensen",
+    "phone": "+1 (967) 531-2298"
+  },
+  {
+    "name": "Carrillo Cain",
+    "phone": "+1 (959) 549-2235"
+  },
+  {
+    "name": "Johnston Armstrong",
+    "phone": "+1 (848) 520-3116"
+  },
+  {
+    "name": "Franco Morrow",
+    "phone": "+1 (821) 583-2051"
+  },
+  {
+    "name": "Shirley Prince",
+    "phone": "+1 (933) 492-2388"
+  },
+  {
+    "name": "Slater Leon",
+    "phone": "+1 (932) 475-2405"
+  },
+  {
+    "name": "Sandoval Gaines",
+    "phone": "+1 (840) 417-3199"
+  },
+  {
+    "name": "Glass Rojas",
+    "phone": "+1 (844) 525-3026"
+  },
+  {
+    "name": "Goodwin Stark",
+    "phone": "+1 (963) 500-3251"
+  },
+  {
+    "name": "Christensen Mooney",
+    "phone": "+1 (994) 574-2387"
+  },
+  {
+    "name": "Ewing Cantu",
+    "phone": "+1 (958) 534-3520"
+  },
+  {
+    "name": "Francis Guy",
+    "phone": "+1 (805) 448-2231"
+  },
+  {
+    "name": "Lyons Ingram",
+    "phone": "+1 (988) 529-3360"
+  },
+  {
+    "name": "Love Morales",
+    "phone": "+1 (866) 595-2586"
+  },
+  {
+    "name": "Sheppard Foreman",
+    "phone": "+1 (966) 456-3992"
+  },
+  {
+    "name": "Roy Bailey",
+    "phone": "+1 (878) 525-3419"
+  },
+  {
+    "name": "Buckner Stewart",
+    "phone": "+1 (982) 573-3749"
+  },
+  {
+    "name": "Teresa Crane",
+    "phone": "+1 (939) 443-2097"
+  },
+  {
+    "name": "Bradshaw Mcfadden",
+    "phone": "+1 (971) 505-2636"
+  },
+  {
+    "name": "Owens Hodges",
+    "phone": "+1 (802) 539-2408"
+  },
+  {
+    "name": "Robbie Velasquez",
+    "phone": "+1 (963) 485-2348"
+  },
+  {
+    "name": "Jennie Schneider",
+    "phone": "+1 (964) 460-3653"
+  },
+  {
+    "name": "Benita Carson",
+    "phone": "+1 (953) 418-2465"
+  },
+  {
+    "name": "Julianne Sargent",
+    "phone": "+1 (894) 450-2666"
+  },
+  {
+    "name": "Hahn Burgess",
+    "phone": "+1 (838) 506-2536"
+  },
+  {
+    "name": "Stanley Webb",
+    "phone": "+1 (839) 441-3947"
+  },
+  {
+    "name": "Jeri Mcclain",
+    "phone": "+1 (898) 531-3163"
+  },
+  {
+    "name": "Phelps Lawrence",
+    "phone": "+1 (976) 492-2793"
+  },
+  {
+    "name": "Reba Willis",
+    "phone": "+1 (984) 499-3330"
+  },
+  {
+    "name": "Nannie Waller",
+    "phone": "+1 (977) 490-3692"
+  },
+  {
+    "name": "Sykes Mcdowell",
+    "phone": "+1 (969) 420-2805"
+  },
+  {
+    "name": "Colon Peck",
+    "phone": "+1 (996) 583-3150"
+  },
+  {
+    "name": "Wendy Bolton",
+    "phone": "+1 (957) 599-2405"
+  },
+  {
+    "name": "Ernestine Berger",
+    "phone": "+1 (811) 594-2838"
+  },
+  {
+    "name": "Weiss Steele",
+    "phone": "+1 (893) 419-2235"
+  },
+  {
+    "name": "Katheryn Hood",
+    "phone": "+1 (974) 515-2636"
+  },
+  {
+    "name": "Joyce Matthews",
+    "phone": "+1 (988) 494-2805"
+  },
+  {
+    "name": "Guthrie Jacobson",
+    "phone": "+1 (875) 442-3510"
+  },
+  {
+    "name": "Lydia Whitney",
+    "phone": "+1 (944) 572-3589"
+  },
+  {
+    "name": "Lester Nieves",
+    "phone": "+1 (983) 545-3876"
+  },
+  {
+    "name": "Edna Wright",
+    "phone": "+1 (854) 532-3824"
+  },
+  {
+    "name": "Scott Deleon",
+    "phone": "+1 (811) 534-2096"
+  },
+  {
+    "name": "Teri Mcneil",
+    "phone": "+1 (922) 461-3467"
+  },
+  {
+    "name": "Shannon Camacho",
+    "phone": "+1 (871) 533-3296"
+  },
+  {
+    "name": "Elliott Callahan",
+    "phone": "+1 (950) 411-2776"
+  },
+  {
+    "name": "Burton Joyner",
+    "phone": "+1 (815) 519-3437"
+  },
+  {
+    "name": "Lawrence Coffey",
+    "phone": "+1 (849) 495-2665"
+  },
+  {
+    "name": "Kelli Acevedo",
+    "phone": "+1 (988) 488-2083"
+  },
+  {
+    "name": "Elsie Hebert",
+    "phone": "+1 (950) 496-3475"
+  },
+  {
+    "name": "Galloway Wall",
+    "phone": "+1 (844) 599-3939"
+  },
+  {
+    "name": "Cannon Rosa",
+    "phone": "+1 (994) 495-3270"
+  },
+  {
+    "name": "Walton Noble",
+    "phone": "+1 (875) 505-2556"
+  },
+  {
+    "name": "Patel Curtis",
+    "phone": "+1 (983) 592-3946"
+  },
+  {
+    "name": "Melody Cameron",
+    "phone": "+1 (994) 552-3147"
+  },
+  {
+    "name": "Smith Espinoza",
+    "phone": "+1 (873) 475-3453"
+  },
+  {
+    "name": "Kirk Mayo",
+    "phone": "+1 (807) 428-3062"
+  },
+  {
+    "name": "Lawson Gray",
+    "phone": "+1 (908) 548-2624"
+  },
+  {
+    "name": "Dale Pugh",
+    "phone": "+1 (906) 449-2843"
+  },
+  {
+    "name": "Lupe Burt",
+    "phone": "+1 (903) 489-2979"
+  },
+  {
+    "name": "Callahan Love",
+    "phone": "+1 (880) 498-2861"
+  },
+  {
+    "name": "Moreno Tate",
+    "phone": "+1 (933) 535-3382"
+  }
+]
+

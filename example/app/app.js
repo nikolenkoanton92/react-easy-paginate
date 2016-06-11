a498d50326201f492319ada96b9b70f99e1c4b38
Anton Nikolenko <nikolenkoanton92@gmail.com>
2016-06-11 00:01:56 -0700

    add to example fake data, css style for react-easy-paginate

--- example/app/app.js
+++ example/app/app.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
+import ReactEasyPaginate from 'react-easy-paginate'
+import 'react-easy-paginate.css'
+import phoneBookData from './data/phone-book'
 
 const App = React.createClass({
-
 
   render() {
     return (
       <div className="container">
       <h1>Example App</h1>
+      <div id="react-easy-paginate">
+        <ReactEasyPaginate pageTotal={10} />
+        </div>
       </div>
       )
   }
